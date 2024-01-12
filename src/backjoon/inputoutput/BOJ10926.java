e6328ed3db7becda1daa6a75450ef0b321ebced4
rtaeho <ryou0920@naver.com>
2024-01-12 18:08:28 +0900

    .

--- src/backjoon/inputoutput/BOJ10926.java
+++ src/backjoon/inputoutput/BOJ10926.java
@@ -18,8 +18,13 @@ baekjoon??!
 */
 package backjoon.inputoutput;
 
+import java.util.Scanner;
+
 public class BOJ10926 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+
+        System.out.println(s + "??!");
     }
 }
