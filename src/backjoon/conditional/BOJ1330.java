d2fe6f413211c33691096256c605f8a5129cd4ff
rtaeho <ryou0920@naver.com>
2024-01-14 17:30:59 +0900

    조건문

--- src/backjoon/conditional/BOJ1330.java
+++ src/backjoon/conditional/BOJ1330.java
@@ -29,8 +29,18 @@ A와 B가 같은 경우에는 '=='를 출력한다.
  */
 package backjoon.conditional;
 
+import java.util.Scanner;
+
 public class BOJ1330 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        if (a > b) System.out.println(">");
+        else if (a < b) System.out.println("<");
+        else System.out.println("==");
     }
 }
