c948220636dafbeb3735285f9071ccf1613433c3
rtaeho <ryou0920@naver.com>
2024-01-15 17:57:40 +0900

    반복분 절반

--- src/backjoon/loop/BOJ25304.java
+++ src/backjoon/loop/BOJ25304.java
@@ -55,8 +55,27 @@ No
  */
 package backjoon.loop;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class BOJ25304 {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int total = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            sum += a * b;
+        }
+        if (sum == total) System.out.println("Yes");
+        else System.out.println("No");
     }
 }
