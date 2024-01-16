09243f34f75afb6e1d89a983c42dea0f7081ecd0
rtaeho <ryou0920@naver.com>
2024-01-16 18:03:22 +0900

    .

--- src/backjoon/loop/BOJ15552.java
+++ src/backjoon/loop/BOJ15552.java
@@ -36,5 +36,28 @@ Python을 사용하고 있다면, input 대신 sys.stdin.readline을 사용할 �
  */
 package backjoon.loop;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class BOJ15552 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine()," ");
+            sb.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())).append('\n');
+        }
+        br.close();
+
+        System.out.println(sb);
+
+    }
 }
