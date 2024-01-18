b0075a742be6e047e27f9db79826578388e81252
rtaeho <ryou0920@naver.com>
2024-01-18 17:04:03 +0900

    .
    .

--- src/backjoon/loop/BOJ2438.java
+++ src/backjoon/loop/BOJ2438.java
@@ -20,8 +20,20 @@
  */
 package backjoon.loop;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class BOJ2438 {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.print('\n');
+        }
     }
 }
