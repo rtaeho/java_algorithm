21298f2bfe30f7420cdbdaa5ce4171fa6f3f43a3
rtaeho <ryou0920@naver.com>
2024-01-19 01:35:31 +0900

    .

--- src/backjoon/onedimentionarrary/BOJ10807.java
+++ src/backjoon/onedimentionarrary/BOJ10807.java
@@ -24,5 +24,25 @@
  */
 package backjoon.onedimentionarrary;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class BOJ10807 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] num = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+        int v = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        for (int j : num) {
+            if (j == v) cnt++;
+        }
+        System.out.println(cnt);
+    }
 }
