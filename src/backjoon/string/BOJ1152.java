6bf8182e6b23b41b0579b0553d89a6d44990f2c6
rtaeho <ryou0920@naver.com>
2024-01-20 17:45:35 +0900

    .
    공백이 앞 뒤에 있는 경우 공백 = 32임을 이용하여 System.in.read를 이용하여 바로 풀기

--- src/backjoon/string/BOJ1152.java
+++ src/backjoon/string/BOJ1152.java
@@ -34,5 +34,36 @@ public class BOJ1152 {
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         System.out.print(st.countTokens());
+        /*
+        public static void main(String[] args) throws IOException {
+
+		int count = 0;
+		int pre_str = 32;	// 공백을 의미한다.
+		int str ;
+
+
+		while(true) {
+			str = System.in.read();
+
+			// 입력받은 문자가 공백일 때,
+			if(str == 32) {
+				// 이전의 문자가 공백이 아니면
+				if(pre_str != 32) count++;
+			}
+
+			// 입력받은 문자가 개행일때 ('\n')
+			else if(str == 10) {
+				// 이전의 문자가 공백이 아니면
+				if(pre_str != 32) count++;
+				break;
+			}
+
+			pre_str = str;
+
+		}
+
+		System.out.println(count);
+	}
+         */
     }
 }
