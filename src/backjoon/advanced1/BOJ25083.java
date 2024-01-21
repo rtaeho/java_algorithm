c9b1e004060e4eb0ca23d03e49a8c31c83e07d1e
rtaeho <ryou0920@naver.com>
2024-01-21 17:16:59 +0900

    .

--- src/backjoon/advanced1/BOJ25083.java
+++ src/backjoon/advanced1/BOJ25083.java
@@ -21,4 +21,12 @@ r`-_   ,'  ,/
 package backjoon.advanced1;
 
 public class BOJ25083 {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |\n");
+    }
 }
