1bcb92766499114627b41267dbb74515c054c957
rtaeho <ryou0920@naver.com>
2024-01-22 18:21:37 +0900

    이차원배열 끝

--- src/backjoon/twodimentionalarray/BOJ2738.java
+++ src/backjoon/twodimentionalarray/BOJ2738.java
@@ -27,7 +27,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-58297667
 
 public class BOJ2738 {
     public static void main(String[] args) throws IOException {
