b3cfa04116c288f0e06a427f2d1682a3bc65017b
rtaeho <ryou0920@naver.com>
2024-01-23 17:48:04 +0900

    .
    그리디 알고리즘 사용

--- src/backjoon/generalmath/BOJ2720.java
+++ src/backjoon/generalmath/BOJ2720.java
@@ -39,9 +39,28 @@ public class BOJ2720 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int Quarter = 25;
+        int Dime = 10;
+        int Nickel = 5;
+        int Penny = 1;
 
         for (int i = 0; i < T; i++) {
-            
+            int C = Integer.parseInt(br.readLine());
+            sb.append(C / Quarter + " ");
+            C %= Quarter;
+
+            sb.append(C / Dime + " ");
+            C %= Dime;
+
+            sb.append(C / Nickel + " ");
+            C %= Nickel;
+
+            sb.append(C / Penny + "\n");
         }
+        System.out.print(sb);
+        br.close();
     }
 }
+
