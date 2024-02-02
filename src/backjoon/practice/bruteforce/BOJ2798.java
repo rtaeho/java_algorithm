0d21fc44a7d56a88c06e7d370f67c35767294eee
rtaeho <ryou0920@naver.com>
2024-02-02 16:16:05 +0900

    브루트 포스 시작
    이 문제 전에 했던 조합을 사용하면 편한 문제였다. nC3과 같은 삼중 반복문.

--- src/backjoon/practice/bruteforce/BOJ2798.java
+++ src/backjoon/practice/bruteforce/BOJ2798.java
@@ -31,8 +31,42 @@ N장의 카드에 써져 있는 숫자가 주어졌을 때, M을 넘지 않으�
  */
 package backjoon.practice.bruteforce;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class BOJ2798 {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int result = search(arr, N, M);
+        System.out.println(result);
+        br.close();
+    }
+
+    static int search(int[] arr, int N, int M) {
+        int result = 0;
+
+        for (int i = 0; i < N - 2; i++) {
+            for (int j = i + 1; j < N - 1; j++) {
+                for (int k = j + 1; k < N; k++) {
+                    int temp = arr[i] + arr[j] + arr[k];
+                    if (M == temp) return temp;
+                    if (result < temp && temp < M) result = temp;
+                }
+            }
+        }
+        return result;
     }
 }
