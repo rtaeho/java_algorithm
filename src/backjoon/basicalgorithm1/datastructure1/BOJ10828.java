34a328535a30da7d393c34e0ac256e4fe2eec169
rtaeho <ryou0920@naver.com>
2024-02-04 16:28:15 +0900

    내부 스택함수 사용

--- src/backjoon/basicalgorithm1/datastructure1/BOJ10828.java
+++ src/backjoon/basicalgorithm1/datastructure1/BOJ10828.java
@@ -150,3 +150,52 @@ public class BOJ10828 {
     }
 
 }
+
+/*
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+        Stack<Integer> stack = new Stack<Integer>();
+
+        for (int i = 0; i < n; i++) {
+
+            StringTokenizer st = new StringTokenizer(bf.readLine());
+            String m = st.nextToken();
+            if (m.equals("push")) {
+                int c = Integer.parseInt(st.nextToken());
+                stack.push(c);
+
+            } else if (m.equals("pop")) {
+                if (stack.isEmpty()) {
+                    System.out.println("-1");
+                } else
+                    System.out.println(stack.pop());
+
+            } else if (m.equals("size")) {
+                System.out.println(stack.size());
+            } else if (m.equals("empty")) {
+                if (stack.isEmpty()) {
+                    System.out.println("1");
+
+                } else {
+                    System.out.println("0");
+                }
+            } else if (m.equals("top")) {
+                if (stack.isEmpty()) {
+                    System.out.println("-1");
+                } else
+                    System.out.println(stack.peek());
+            }
+
+        }
+    }
+
+}*/
