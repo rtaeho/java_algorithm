3af335a0ed777830f6890c4d389d0a11af3969f0
rtaeho <ryou0920@naver.com>
2024-02-14 14:08:38 +0900

    파일명 변경

---
+++ src/baekjoon/basicalgorithm1/datastructure1practice/BOJ17299.java
@@ -0,0 +1,62 @@
+/*
+문제
+크기가 N인 수열 A = A1, A2, ..., AN이 있다. 수열의 각 원소 Ai에 대해서 오등큰수 NGF(i)를 구하려고 한다.
+
+Ai가 수열 A에서 등장한 횟수를 F(Ai)라고 했을 때, Ai의 오등큰수는 오른쪽에 있으면서 수열 A에서 등장한 횟수가 F(Ai)보다 큰 수 중에서 가장 왼쪽에 있는 수를 의미한다. 그러한 수가 없는 경우에 오등큰수는 -1이다.
+
+예를 들어, A = [1, 1, 2, 3, 4, 2, 1]인 경우 F(1) = 3, F(2) = 2, F(3) = 1, F(4) = 1이다. A1의 오른쪽에 있으면서 등장한 횟수가 3보다 큰 수는 없기 때문에, NGF(1) = -1이다. A3의 경우에는 A7이 오른쪽에 있으면서 F(A3=2) < F(A7=1) 이기 때문에, NGF(3) = 1이다. NGF(4) = 2, NGF(5) = 2, NGF(6) = 1 이다.
+
+입력
+첫째 줄에 수열 A의 크기 N (1 ≤ N ≤ 1,000,000)이 주어진다. 둘째에 수열 A의 원소 A1, A2, ..., AN (1 ≤ Ai ≤ 1,000,000)이 주어진다.
+
+출력
+총 N개의 수 NGF(1), NGF(2), ..., NGF(N)을 공백으로 구분해 출력한다.
+
+예제 입력 1
+7
+1 1 2 3 4 2 1
+예제 출력 1
+-1 -1 1 2 2 1 -1
+ */
+package baekjoon.basicalgorithm1.datastructure1practice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class BOJ17299 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int[] cnt = new int[1000001];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            cnt[arr[i]]++;
+        }
+        Stack<Integer> stack = new Stack<>();
+        int[] ans = new int[N];
+        for (int i = 0; i < N; i++) {
+            while (!stack.empty() && cnt[arr[i]] > cnt[arr[stack.peek()]]) {
+                ans[stack.pop()] = arr[i];
+            }
+            stack.push(i);
+        }
+
+        while (!stack.empty()) {
+            ans[stack.pop()] = -1;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(ans[i]).append(" ");
+        }
+        System.out.println(sb);
+        br.close();
+    }
+
+}
---
+++ src/baekjoon/practice/advanced1/BOJ25083.java
@@ -0,0 +1,32 @@
+/*
+문제
+아래 예제와 같이 새싹을 출력하시오.
+
+입력
+입력은 없다.
+
+출력
+새싹을 출력한다.
+
+예제 입력 1
+예제 출력 1
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+
+ */
+package baekjoon.practice.advanced1;
+
+public class BOJ25083 {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |\n");
+    }
+}
