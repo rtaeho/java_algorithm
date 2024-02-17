dbf3c037e15d04d36ce1174aab0e545a99a32069
rtaeho <ryou0920@naver.com>
2024-02-17 16:49:32 +0900

    네 수

---
+++ src/baekjoon/basicalgorithm1/datastructure1refer/BOJ10824.java
@@ -0,0 +1,39 @@
+/*
+문제
+네 자연수 A, B, C, D가 주어진다. 이때, A와 B를 붙인 수와 C와 D를 붙인 수의 합을 구하는 프로그램을 작성하시오.
+
+두 수 A와 B를 합치는 것은 A의 뒤에 B를 붙이는 것을 의미한다. 즉, 20과 30을 붙이면 2030이 된다.
+
+입력
+첫째 줄에 네 자연수 A, B, C, D가 주어진다. (1 ≤ A, B, C, D ≤ 1,000,000)
+
+출력
+A와 B를 붙인 수와 C와 D를 붙인 수의 합을 출력한다.
+
+예제 입력 1
+10 20 30 40
+예제 출력 1
+4060
+ */
+package baekjoon.basicalgorithm1.datastructure1refer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ10824 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String a = st.nextToken();
+        String b = st.nextToken();
+        String c = st.nextToken();
+        String d = st.nextToken();
+        String ab = a + b;
+        String cd = c + d;
+        long abcd = Long.parseLong(ab) + Long.parseLong(cd);
+        System.out.println(abcd);
+
+    }
+}
