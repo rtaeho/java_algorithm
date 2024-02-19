7ce2eec88e74f317ff90f8c834f553747225ad00
rtaeho <ryou0920@naver.com>
2024-02-19 15:56:16 +0900

    .

--- src/baekjoon/basicalgorithm1/math1/BOJ2609.java
+++ src/baekjoon/basicalgorithm1/math1/BOJ2609.java
@@ -43,4 +43,30 @@ public class BOJ2609 {
         }
         System.out.println(i);
     }
+    /*
+    public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+
+		int d = gcd(a, b);	// 최대공약수
+
+		System.out.println(d);
+		System.out.println(a * b / d);
+
+	}
+
+	// 최대공약수 재귀 방식
+	public static int gcd(int a, int b) {
+		if (b == 0)
+			return a;
+
+		// GCD(a, b) = GCD(b, r)이므로 (r = a % b)
+		return gcd(b, a % b);
+	}
+     */
 }
