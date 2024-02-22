03cf66b20c5f976fb973659c257e922fc23d49ae
rtaeho <ryou0920@naver.com>
2024-02-22 23:43:11 +0900

    [Bronze V] Title: 팩토리얼, Time: 128 ms, Memory: 14076 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,36 @@
+/*
+문제
+0보다 크거나 같은 정수 N이 주어진다. 이때, N!을 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 정수 N(0 ≤ N ≤ 12)이 주어진다.
+
+출력
+첫째 줄에 N!을 출력한다.
+
+예제 입력 1 
+10
+예제 출력 1 
+3628800
+예제 입력 2 
+0
+예제 출력 2 
+1
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(factorial(N));
+        br.close();
+    }
+
+    public static int factorial(int N) {
+        if (N <= 1) return 1;
+        return N * factorial(N - 1);
+    }
+}
