3bf4b605f1c6ca9e75a0e016813e643b59502e82
rtaeho <ryou0920@naver.com>
2024-02-23 14:14:21 +0900

    팩토리얼 0의 개수
    단순 입력 후 0을 카운트하면 시간이 너무 오래 걸리기에 팩토리얼의 원리 분석 5의 제곱수 개수만큼인 걸 파악

---
+++ src/baekjoon/basicalgorithm1/math1/BOJ1676.java
@@ -0,0 +1,41 @@
+/*
+문제
+N!에서 뒤에서부터 처음 0이 아닌 숫자가 나올 때까지 0의 개수를 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 N이 주어진다. (0 ≤ N ≤ 500)
+
+출력
+첫째 줄에 구한 0의 개수를 출력한다.
+
+예제 입력 1
+10
+예제 출력 1
+2
+예제 입력 2
+3
+예제 출력 2
+0
+
+ */
+package baekjoon.basicalgorithm1.math1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ1676 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+        int count = 0;
+
+        while (num >= 5) {
+            count += num / 5;
+            num /= 5;
+        }
+        System.out.println(count);
+        br.close();
+    }
+}
