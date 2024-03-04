66673f7d25589fdbf6d926a80ed3bf5d7a4965bc
rtaeho <ryou0920@naver.com>
2024-03-04 17:25:51 +0900

    .

--- src/baekjoon/basicalgorithm1/dynamicprogramming1/BOJ11726.java
+++ src/baekjoon/basicalgorithm1/dynamicprogramming1/BOJ11726.java
@@ -23,8 +23,22 @@
  */
 package baekjoon.basicalgorithm1.dynamicprogramming1;
 
-public class BOJ11726 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class BOJ11726 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[1001];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+        }
+        System.out.println(dp[n]);
+        br.close();
     }
 }
