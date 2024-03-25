f186dca4e188fe0f281156320fc553266a46a70f
rtaeho <ryou0920@naver.com>
2024-03-25 16:09:18 +0900

    [Silver IV] Title: 듣보잡, Time: 328 ms, Memory: 26032 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,58 @@
+/*
+문제
+김진영이 듣도 못한 사람의 명단과, 보도 못한 사람의 명단이 주어질 때, 듣도 보도 못한 사람의 명단을 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 듣도 못한 사람의 수 N, 보도 못한 사람의 수 M이 주어진다. 이어서 둘째 줄부터 N개의 줄에 걸쳐 듣도 못한 사람의 이름과, N+2째 줄부터 보도 못한 사람의 이름이 순서대로 주어진다. 이름은 띄어쓰기 없이 알파벳 소문자로만 이루어지며, 그 길이는 20 이하이다. N, M은 500,000 이하의 자연수이다.
+
+듣도 못한 사람의 명단에는 중복되는 이름이 없으며, 보도 못한 사람의 명단도 마찬가지이다.
+
+출력
+듣보잡의 수와 그 명단을 사전순으로 출력한다.
+
+예제 입력 1
+3 4
+ohhenrie
+charlie
+baesangwook
+obama
+baesangwook
+ohhenrie
+clinton
+예제 출력 1
+2
+baesangwook
+ohhenrie
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        HashSet<String> hash = new HashSet<>();
+
+        for (int i = 0; i < N; i++) {
+            hash.add(br.readLine());
+        }
+        ArrayList<String> arr = new ArrayList<String>();
+
+        for (int i = 0; i < M; i++) {
+            String str = br.readLine();
+            if (hash.contains(str)) {
+                arr.add(str);
+            }
+        }
+        Collections.sort(arr);
+        System.out.println(arr.size());
+        for (String s : arr) {
+            System.out.println(s);
+        }
+        br.close();
+    }
+}
