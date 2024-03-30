1ce364fc657858f22672ff3e4f65c3e95a09f36c
rtaeho <ryou0920@naver.com>
2024-03-30 15:25:09 +0900

    2485

--- src/baekjoon/practice/factorsmultiplesanddecimals/BOJ2581.java
+++ src/baekjoon/practice/factorsmultiplesanddecimals/BOJ2581.java
@@ -32,7 +32,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class BOJ25i81 {
+public class BOJ2581 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int M = Integer.parseInt(br.readLine());
