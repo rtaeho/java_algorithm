eab6cc4af9ee0caea4fb027eab62ff0705b11c8d
rtaeho <ryou0920@naver.com>
2024-04-19 14:05:46 +0900

    [Gold V] Title: 합분해, Time: 124 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Gold/2225. 합분해/합분해.java
@@ -0,0 +1,55 @@
+/*
+문제
+0부터 N까지의 정수 K개를 더해서 그 합이 N이 되는 경우의 수를 구하는 프로그램을 작성하시오.
+
+덧셈의 순서가 바뀐 경우는 다른 경우로 센다(1+2와 2+1은 서로 다른 경우). 또한 한 개의 수를 여러 번 쓸 수도 있다.
+
+입력
+첫째 줄에 두 정수 N(1 ≤ N ≤ 200), K(1 ≤ K ≤ 200)가 주어진다.
+
+출력
+첫째 줄에 답을 1,000,000,000으로 나눈 나머지를 출력한다.
+
+예제 입력 1
+20 2
+예제 출력 1
+21
+예제 입력 2
+6 4
+예제 출력 2
+84
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static int MOD = 1000000000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        br.close();
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[][] dp = new int[N + 1][K + 1];
+
+        for (int i = 0; i <= N; i++) {
+            dp[i][0] = 0;
+            dp[i][1] = 1;
+        }
+
+        for (int i = 0; i <= K; i++) {
+            dp[1][i] = i;
+        }
+
+        for (int i = 2; i <= N; i++) {
+            for (int j = 2; j <= K; j++) {
+                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % MOD;
+            }
+        }
+
+        System.out.println(dp[N][K] % MOD);
+    }
+}
