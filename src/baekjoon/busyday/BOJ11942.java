73ef7a954502cfa2f4096d053431088c32b6843e
rtaeho <ryou0920@naver.com>
2024-05-02 14:25:24 +0900

    11942

---
+++ src/baekjoon/busyday/BOJ11942.java
@@ -0,0 +1,18 @@
+/*
+문제
+고려대학교는 사랑입니다.
+
+출력
+첫째 줄에 “고려대학교”를 출력하세요.
+
+예제 입력 1
+예제 출력 1
+고려대학교
+ */
+package baekjoon.busyday;
+
+public class BOJ11942 {
+    public static void main(String[] args) {
+        System.out.println("고려대학교");
+    }
+}
