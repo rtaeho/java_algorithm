9c6e441192eb97782f63307d71edad9dea543b8a
rtaeho <ryou0920@naver.com>
2024-05-02 12:41:30 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 124 ms, Memory: 13872 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.java
@@ -0,0 +1,16 @@
+/*
+문제
+고려대학교는 사랑입니다.
+
+출력
+첫째 줄에 “고려대학교”를 출력하세요.
+
+예제 입력 1
+예제 출력 1
+고려대학교
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("고려대학교");
+    }
+}
