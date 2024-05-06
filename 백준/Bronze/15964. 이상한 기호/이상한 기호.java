3d4a6514086ed0a3e079ef27dd1b3d4c3743c88f
rtaeho <ryou0920@naver.com>
2024-05-06 18:33:20 +0900

    [Bronze V] Title: 이상한 기호, Time: 132 ms, Memory: 14424 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.java
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -31,14 +31,15 @@ A, B ≤ 1,000
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
-        System.out.println((A + B) * (A - B));
+        BigInteger A = new BigInteger(st.nextToken());
+        BigInteger B = new BigInteger(st.nextToken());
+        System.out.println(A.add(B).multiply(A.subtract(B)));
     }
 }
