c698c6a29d00a7e366d725f62b533c7c0f1b941c
rtaeho <ryou0920@naver.com>
2024-05-16 13:27:02 +0900

    [Bronze V] Title: 나는 누구인가, Time: 112 ms, Memory: 13860 KB -BaekjoonHub

---
+++ 백준/Bronze/15733. 나는 누구인가/나는 누구인가.java
@@ -0,0 +1,19 @@
+/*
+문제
+당신은 지금 코딩을 하고있다. 그럼 당신은 어떤 사람인가.
+
+입력
+없다.
+
+출력
+“I'm Sexy”를 출력한다.
+
+예제 입력 1
+예제 출력 1
+I'm Sexy
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("I'm Sexy");
+    }
+}
