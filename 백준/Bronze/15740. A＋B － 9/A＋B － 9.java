d8ff1c1a2845b2d082ae7f3b2a8c7c0120484333
rtaeho <ryou0920@naver.com>
2024-05-19 14:34:40 +0900

    [Bronze V] Title: A+B - 9, Time: 244 ms, Memory: 17216 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15740. A＋B － 9/A＋B － 9.java
+++ 백준/Bronze/15740. A＋B － 9/A＋B － 9.java
@@ -65,8 +65,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        BigInteger A = BigInteger.valueOf(Long.parseLong(st.nextToken()));
-        BigInteger B = BigInteger.valueOf(Long.parseLong(st.nextToken()));
-        System.out.println(A.add(B));
+        BigInteger a = new BigInteger(st.nextToken());
+        BigInteger b = new BigInteger(st.nextToken());
+        System.out.print(a.add(b));
     }
 }
