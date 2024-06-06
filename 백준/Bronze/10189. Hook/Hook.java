67e8bb4f1aa37e893d5c1c2d7f81fbce86251884
rtaeho <ryou0920@naver.com>
2024-06-06 13:59:40 +0900

    [Bronze V] Title: Hook, Time: 112 ms, Memory: 13868 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.java
@@ -0,0 +1,22 @@
+/*
+문제
+Print out the word Hook as shown below.
+
+출력
+Print out the word Hook as shown below.
+
+예제 입력 1
+예제 출력 1
+#  # #### #### #  #
+#### #  # #  # # #
+#### #  # #  # # #
+#  # #### #### #  #
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("#  # #### #### #  #\n" +
+                "#### #  # #  # # #\n" +
+                "#### #  # #  # # #\n" +
+                "#  # #### #### #  #");
+    }
+}
