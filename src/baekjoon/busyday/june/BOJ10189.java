1f99aa7ef76c91742b58a0f3cc3f4cf65b75c552
rtaeho <ryou0920@naver.com>
2024-06-06 14:00:01 +0900

    10189

---
+++ src/baekjoon/busyday/june/BOJ10189.java
@@ -0,0 +1,24 @@
+/*
+문제
+Print out the word Hook as shown below.
+
+출력
+Print out the word Hook as shown below.
+
+예제 입력 1
+예제 출력 1
+#  # #### #### #  #
+#### #  # #  # # #
+#### #  # #  # # #
+#  # #### #### #  #
+ */
+package baekjoon.busyday.june;
+
+public class BOJ10189 {
+    public static void main(String[] args) {
+        System.out.println("#  # #### #### #  #\n" +
+                "#### #  # #  # # #\n" +
+                "#### #  # #  # # #\n" +
+                "#  # #### #### #  #");
+    }
+}
