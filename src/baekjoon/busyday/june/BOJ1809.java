0efec05a205eef7d6c0340e7c638f5dcccbabd54
rtaeho <ryou0920@naver.com>
2024-06-11 22:31:15 +0900

    1809

---
+++ src/baekjoon/busyday/june/BOJ1809.java
@@ -0,0 +1,24 @@
+/*
+문제
+You’ve decided to buy a farm and start a new life. To pass some time while you wait for the title of the land to go through, produce the following ascii cow.
+
+예제 입력 1
+예제 출력 1
+(___)
+(o o)____/
+ @@      \
+  \ ____,/
+  //   //
+ ^^   ^^
+ */
+package baekjoon.busyday.june;
+
+/*
+(___)
+(o o)____/
+ @@      \
+  \ ____,/
+  //   //
+ ^^   ^^
+이대로 출력하면 답
+ */
