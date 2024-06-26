eaded6f1dbf2aac6e900463422525e7df03373f5
rtaeho <ryou0920@naver.com>
2024-06-26 21:22:10 +0900

    [Bronze V] Title: Zadanie próbne 2, Time: 132 ms, Memory: 14360 KB -BaekjoonHub

---
+++ 백준/Bronze/8871. Zadanie próbne 2/Zadanie próbne 2.java
@@ -0,0 +1,26 @@
+/*
+문제
+Wyobraźmy sobie przez chwilę, że w tym roku konkurs SKI'10 składa się z n punktowanych rund i jednej rundy próbnej. Ile zgodnie z regulaminem może się pojawić zadań w czasie całych zawodów?
+
+입력
+W pierwszej i jedynej linii znajduje się liczba naturalna n (1<=n<=1000).
+
+출력
+Twój program powinien wypisać dwie liczby oddzielone pojedynczym odstępem. Pierwsza liczba to minimalna liczba zadań jaka może pojawić się podczas n rund punktowanych i jednej rundy próbnej w trakcie SKI'10. Druga liczba to maksymalna liczba zadań w tym konkursie.
+
+예제 입력 1
+5
+예제 출력 1
+12 18
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.printf("%d %d", (n + 1) * 2, (n + 1) * 3);
+    }
+}
