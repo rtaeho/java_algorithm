ee43c64785b08551fc30b7a27721c288b4f1d6ba
rtaeho <ryou0920@naver.com>
2024-08-15 15:02:44 +0900

    [Bronze V] Title: ゾロ目 (Same Numbers), Time: 100 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/27324. ゾロ目 （Same Numbers）/ゾロ目 （Same Numbers）.java
@@ -0,0 +1,39 @@
+/*
+문제
+十進法で 2 桁の整数 N が与えられる．
+
+N の十の位の数字と一の位の数字が同じである場合は 1 を，そうでない場合は 0 を出力せよ．
+
+입력
+入力は以下の形式で標準入力から与えられる．
+
+N
+출력
+N の十の位の数字と一の位の数字が同じである場合は 1 を，そうでない場合は 0 を出力せよ．
+
+제한
+10 ≦ N ≦ 99．
+N は整数である．
+예제 입력 1
+22
+예제 출력 1
+1
+22 は十の位が 2 ，一の位が 2 と同じであるので，1 を出力する．
+
+예제 입력 2
+10
+예제 출력 2
+0
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(n / 10 == n % 10 ? 1 : 0);
+    }
+}
