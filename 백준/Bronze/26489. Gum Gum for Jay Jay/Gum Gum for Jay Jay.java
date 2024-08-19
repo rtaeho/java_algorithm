39e87027884a0f5b4ad69d627d11733a3b77d686
rtaeho <ryou0920@naver.com>
2024-08-19 16:48:23 +0900

    [Bronze V] Title: Gum Gum for Jay Jay, Time: 96 ms, Memory: 14068 KB -BaekjoonHub

---
+++ 백준/Bronze/26489. Gum Gum for Jay Jay/Gum Gum for Jay Jay.java
@@ -0,0 +1,42 @@
+/*
+문제
+You are lost in the museum and keep walking by a giant rock head that says “gum gum for jay jay” each time you walk by. Print out the number of times you have walked by the giant rock head after reading in the data file.
+
+입력
+The data file will contain an unknown number of lines.
+
+출력
+Print out the number of lines in the data file.
+
+예제 입력 1
+gum gum for jay jay
+gum gum for jay jay
+gum gum for jay jay
+gum gum for jay jay
+gum gum for jay jay
+gum gum for jay jay
+gum gum for jay jay
+gum gum for jay jay
+gum gum for jay jay
+gum gum for jay jay
+gum gum for jay jay
+예제 출력 1
+11
+
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cnt = 0;
+        while (true) {
+            String tmp = br.readLine();
+            if (tmp == null) break;
+            cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
