32eecc78981f7daf0f87fc47afbf75ac13a8fc53
rtaeho <ryou0920@naver.com>
2024-09-25 22:50:45 +0900

    Create BOJ1182.java

---
+++ src/baekjoon/basic_algorithm_2/bruteforce_BitMask/BOJ1182.java
@@ -0,0 +1,53 @@
+/*
+문제
+N개의 정수로 이루어진 수열이 있을 때, 크기가 양수인 부분수열 중에서 그 수열의 원소를 다 더한 값이 S가 되는 경우의 수를 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 정수의 개수를 나타내는 N과 정수 S가 주어진다. (1 ≤ N ≤ 20, |S| ≤ 1,000,000) 둘째 줄에 N개의 정수가 빈 칸을 사이에 두고 주어진다. 주어지는 정수의 절댓값은 100,000을 넘지 않는다.
+
+출력
+첫째 줄에 합이 S가 되는 부분수열의 개수를 출력한다.
+
+예제 입력 1
+5 0
+-7 -3 -2 5 8
+예제 출력 1
+1
+ */
+package baekjoon.basic_algorithm_2.bruteforce_BitMask;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class BOJ1182 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] firstLine = br.readLine().split(" ");
+        int N = Integer.parseInt(firstLine[0]);
+        int S = Integer.parseInt(firstLine[1]);
+
+        int[] numbers = new int[N];
+        String[] numberStrings = br.readLine().split(" ");
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(numberStrings[i]);
+        }
+
+        int count = 0;
+
+        for (int i = 1; i < (1 << N); i++) {
+            int sum = 0;
+            for (int j = 0; j < N; j++) {
+                if ((i & (1 << j)) != 0) {
+                    sum += numbers[j];
+                }
+            }
+            if (sum == S) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
