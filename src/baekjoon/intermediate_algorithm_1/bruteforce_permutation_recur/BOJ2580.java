0069b75d7a0d50e63bf1e5a48e8305fb9ad6e798
rtaeho <ryou0920@naver.com>
2024-10-29 20:16:23 +0900

    Create BOJ2580.java

---
+++ src/baekjoon/intermediate_algorithm_1/bruteforce_permutation_recur/BOJ2580.java
@@ -0,0 +1,148 @@
+/*
+문제
+스도쿠는 18세기 스위스 수학자가 만든 '라틴 사각형'이랑 퍼즐에서 유래한 것으로 현재 많은 인기를 누리고 있다. 이 게임은 아래 그림과 같이 가로, 세로 각각 9개씩 총 81개의 작은 칸으로 이루어진 정사각형 판 위에서 이뤄지는데, 게임 시작 전 일부 칸에는 1부터 9까지의 숫자 중 하나가 쓰여 있다.
+
+
+
+나머지 빈 칸을 채우는 방식은 다음과 같다.
+
+각각의 가로줄과 세로줄에는 1부터 9까지의 숫자가 한 번씩만 나타나야 한다.
+굵은 선으로 구분되어 있는 3x3 정사각형 안에도 1부터 9까지의 숫자가 한 번씩만 나타나야 한다.
+위의 예의 경우, 첫째 줄에는 1을 제외한 나머지 2부터 9까지의 숫자들이 이미 나타나 있으므로 첫째 줄 빈칸에는 1이 들어가야 한다.
+
+
+
+또한 위쪽 가운데 위치한 3x3 정사각형의 경우에는 3을 제외한 나머지 숫자들이 이미 쓰여있으므로 가운데 빈 칸에는 3이 들어가야 한다.
+
+
+
+이와 같이 빈 칸을 차례로 채워 가면 다음과 같은 최종 결과를 얻을 수 있다.
+
+
+
+게임 시작 전 스도쿠 판에 쓰여 있는 숫자들의 정보가 주어질 때 모든 빈 칸이 채워진 최종 모습을 출력하는 프로그램을 작성하시오.
+
+입력
+아홉 줄에 걸쳐 한 줄에 9개씩 게임 시작 전 스도쿠판 각 줄에 쓰여 있는 숫자가 한 칸씩 띄워서 차례로 주어진다. 스도쿠 판의 빈 칸의 경우에는 0이 주어진다. 스도쿠 판을 규칙대로 채울 수 없는 경우의 입력은 주어지지 않는다.
+
+출력
+모든 빈 칸이 채워진 스도쿠 판의 최종 모습을 아홉 줄에 걸쳐 한 줄에 9개씩 한 칸씩 띄워서 출력한다.
+
+스도쿠 판을 채우는 방법이 여럿인 경우는 그 중 하나만을 출력한다.
+
+제한
+12095번 문제에 있는 소스로 풀 수 있는 입력만 주어진다.
+C++14: 80ms
+Java: 292ms
+PyPy3: 1172ms
+예제 입력 1
+0 3 5 4 6 9 2 7 8
+7 8 2 1 0 5 6 0 9
+0 6 0 2 7 8 1 3 5
+3 2 1 0 4 6 8 9 7
+8 0 4 9 1 3 5 0 6
+5 9 6 8 2 0 4 1 3
+9 1 7 6 5 2 0 8 0
+6 0 3 7 0 1 9 5 2
+2 5 8 3 9 4 7 6 0
+예제 출력 1
+1 3 5 4 6 9 2 7 8
+7 8 2 1 3 5 6 4 9
+4 6 9 2 7 8 1 3 5
+3 2 1 5 4 6 8 9 7
+8 7 4 9 1 3 5 2 6
+5 9 6 8 2 7 4 1 3
+9 1 7 6 5 2 3 8 4
+6 4 3 7 8 1 9 5 2
+2 5 8 3 9 4 7 6 1
+ */
+package baekjoon.intermediate_algorithm_1.bruteforce_permutation_recur;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ2580 {
+    static int[][] board = new int[9][9];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 스도쿠 보드 입력 받기
+        for (int i = 0; i < 9; i++) {
+            String[] input = br.readLine().split(" ");
+            for (int j = 0; j < 9; j++) {
+                board[i][j] = Integer.parseInt(input[j]);
+            }
+        }
+
+        // 스도쿠 보드 채우기
+        solveSudoku(0, 0);
+    }
+
+    // 스도쿠 보드를 채우는 함수
+    static boolean solveSudoku(int row, int col) {
+        // 스도쿠 보드를 다 채웠으면 출력 후 true 반환
+        if (row == 9) {
+            printBoard();
+            return true;
+        }
+
+        // 다음 열로 넘어가기
+        if (col == 9) {
+            return solveSudoku(row + 1, 0);
+        }
+
+        // 이미 숫자가 채워진 칸은 넘어감
+        if (board[row][col] != 0) {
+            return solveSudoku(row, col + 1);
+        }
+
+        // 1부터 9까지의 숫자 시도
+        for (int num = 1; num <= 9; num++) {
+            if (isValid(row, col, num)) {
+                board[row][col] = num; // 숫자 채우기
+                if (solveSudoku(row, col + 1)) {
+                    return true; // 다음 칸으로 진행
+                }
+                board[row][col] = 0; // 백트래킹
+            }
+        }
+        return false; // 유효한 숫자가 없을 경우 false 반환
+    }
+
+    // 숫자를 넣을 수 있는지 확인하는 함수
+    static boolean isValid(int row, int col, int num) {
+        // 행과 열 확인
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num || board[i][col] == num) {
+                return false;
+            }
+        }
+
+        // 3x3 박스 확인
+        int boxRow = (row / 3) * 3;
+        int boxCol = (col / 3) * 3;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[boxRow + i][boxCol + j] == num) {
+                    return false;
+                }
+            }
+        }
+
+        return true; // 유효하면 true 반환
+    }
+
+    // 스도쿠 보드를 출력하는 함수
+    static void printBoard() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                sb.append(board[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb.toString());
+    }
+}
