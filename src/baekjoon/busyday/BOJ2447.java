5fdbbccbe1826609b89d39183aae3429deb508d6
rtaeho <ryou0920@naver.com>
2024-11-11 16:38:24 +0900

    Create BOJ2447.java

---
+++ src/baekjoon/busyday/BOJ2447.java
@@ -0,0 +1,110 @@
+/*
+문제
+재귀적인 패턴으로 별을 찍어 보자. N이 3의 거듭제곱(3, 9, 27, ...)이라고 할 때, 크기 N의 패턴은 N×N 정사각형 모양이다.
+
+크기 3의 패턴은 가운데에 공백이 있고, 가운데를 제외한 모든 칸에 별이 하나씩 있는 패턴이다.
+
+***
+* *
+***
+N이 3보다 클 경우, 크기 N의 패턴은 공백으로 채워진 가운데의 (N/3)×(N/3) 정사각형을 크기 N/3의 패턴으로 둘러싼 형태이다. 예를 들어 크기 27의 패턴은 예제 출력 1과 같다.
+
+입력
+첫째 줄에 N이 주어진다. N은 3의 거듭제곱이다. 즉 어떤 정수 k에 대해 N=3k이며, 이때 1 ≤ k < 8이다.
+
+출력
+첫째 줄부터 N번째 줄까지 별을 출력한다.
+
+예제 입력 1
+27
+예제 출력 1
+***************************
+* ** ** ** ** ** ** ** ** *
+***************************
+***   ******   ******   ***
+* *   * ** *   * ** *   * *
+***   ******   ******   ***
+***************************
+* ** ** ** ** ** ** ** ** *
+***************************
+*********         *********
+* ** ** *         * ** ** *
+*********         *********
+***   ***         ***   ***
+* *   * *         * *   * *
+***   ***         ***   ***
+*********         *********
+* ** ** *         * ** ** *
+*********         *********
+***************************
+* ** ** ** ** ** ** ** ** *
+***************************
+***   ******   ******   ***
+* *   * ** *   * ** *   * *
+***   ******   ******   ***
+***************************
+* ** ** ** ** ** ** ** ** *
+***************************
+ */
+package baekjoon.busyday;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ2447 {
+    static char[][] pattern;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        pattern = new char[N][N];
+
+        // 전체 패턴을 '*'로 초기화
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                pattern[i][j] = '*';
+            }
+        }
+
+        // 공백을 넣어가며 패턴 생성
+        drawPattern(0, 0, N);
+
+        // 결과 출력
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                sb.append(pattern[i][j]);
+            }
+            sb.append('\n');
+        }
+        System.out.print(sb);
+    }
+
+    // 재귀적으로 패턴을 생성하는 함수
+    public static void drawPattern(int x, int y, int size) {
+        if (size == 1) {
+            return;
+        }
+
+        int newSize = size / 3;
+
+        // 가운데 부분을 공백으로 채우기
+        for (int i = x + newSize; i < x + 2 * newSize; i++) {
+            for (int j = y + newSize; j < y + 2 * newSize; j++) {
+                pattern[i][j] = ' ';
+            }
+        }
+
+        // 나머지 8개의 구역에 대해 재귀적으로 다시 패턴 채우기
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == 1 && j == 1) {
+                    continue; // 중앙 부분은 건너뜀
+                }
+                drawPattern(x + i * newSize, y + j * newSize, newSize);
+            }
+        }
+    }
+}
