b38a43eb3e08119363128eb1c737068c62328977
rtaeho <ryou0920@naver.com>
2024-11-22 23:59:16 +0900

    Update BOJ2579.java

--- src/baekjoon/busyday/BOJ2579.java
+++ src/baekjoon/busyday/BOJ2579.java
@@ -63,7 +63,7 @@ public class BOJ2579 {
         if (n >= 2) {
             dp[2] = stairs[1] + stairs[2];
         }
-
+ 
         // 점화식: dp[i] = max(dp[i-2], dp[i-3] + stairs[i-1]) + stairs[i]
         for (int i = 3; i <= n; i++) {
             dp[i] = Math.max(dp[i - 2], dp[i - 3] + stairs[i - 1]) + stairs[i];
