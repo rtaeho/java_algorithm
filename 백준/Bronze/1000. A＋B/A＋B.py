de356f64cf72a644639411a2ef54c97f0ecff0bd
rtaeho <ryou0920@naver.com>
2024-11-22 21:29:10 +0900

    [Bronze V] Title: A+B, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.py
+++ 백준/Bronze/1000. A＋B/A＋B.py
@@ -1,7 +1,11 @@
-string = input()
-# string = 1 2
-a, b = string.split()
-# 1 2 => split => 1, 2
-# a = "1"
-# b = "2"
-print(int(a) + int(b))
+ab = input() # "1 2" -> string(문자열) " "
+a, b = ab.split() # ['1', '2'] -> '1' -> char(문자) ' '
+# a = '1' -> 문자
+# b = '2' -> 문자
+# 문자 -> 숫자
+# 숫자 : 정수, 실수
+# 정수 : Integer
+# 실수 : Float
+print(int(a) + int(b)) # '1' + '2' = '12'
+# split() 공백(space)를 기준으로 자름
+# 1 2
