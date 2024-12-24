2ba83b5639622dff786ed0093456e5984bc08ece
rtaeho <ryou0920@naver.com>
2024-12-24 21:30:19 +0900

    Update BOJ1037.java

--- src/baekjoon/busyday/december/BOJ1037.java
+++ src/baekjoon/busyday/december/BOJ1037.java
@@ -31,10 +31,30 @@
  */
 package baekjoon.busyday.december;
 
+import java.io.*;
+import java.util.*;
+
 public class BOJ1037 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        // 진짜 약수의 개수 입력
+        int count = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        // 진짜 약수 배열 생성
+        int[] divisors = new int[count];
+        for (int i = 0; i < count; i++) {
+            divisors[i] = Integer.parseInt(st.nextToken());
+        }
 
+        // 약수 정렬
+        Arrays.sort(divisors);
+
+        // N 계산: 가장 작은 값 * 가장 큰 값
+        int N = divisors[0] * divisors[count - 1];
+
+        // 결과 출력
+        System.out.println(N);
     }
 }
