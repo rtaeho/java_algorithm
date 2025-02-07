4633ceb6e6e9a77886908ce3b6cc1a849f0de48d
류태호 <ryou0920@naver.com>
2025-02-07 20:29:23 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.py
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.py
@@ -1,17 +1,21 @@
-a, b = map(int, input().split())
-# 1. a > b
-# 2. a < b
-# 3. a = b
-# a = 1, b = 2
-if a > b:
-    print(">")
-elif a < b:
+a = input()
+# a : "1 2"
+a, b = a.split()
+# a : '1', b : '2'
+a = int(a)
+b = int(b)
+# a, b에 비교할 수가 저장
+
+# 1. a가 b보다 큰 경우
+# 2. a가 b보다 작은 경우
+# 3. a와 b가 같은 경우
+
+# if 조건 :
+#   조건이 만족할 경우 실행
+# a = 5, b = 5
+if a < b:
     print("<")
+elif a > b:
+    print(">")
 else:
     print("==")
-# if (조건) :
-#   조건이 참인 경우 할 행동
-# elif (조건) :
-#   조건이 참인 경우 할 행동
-# else :
-#   할 행동
