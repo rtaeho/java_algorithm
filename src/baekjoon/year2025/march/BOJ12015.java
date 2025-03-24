bc312529f315aaffece2b4d08230f458c378c1ff
rtaeho <ryou0920@naver.com>
2025-03-24 22:15:20 +0900

    Create BOJ12015.java

---
+++ src/baekjoon/year2025/march/BOJ12015.java
@@ -0,0 +1,62 @@
+/*
+문제
+수열 A가 주어졌을 때, 가장 긴 증가하는 부분 수열을 구하는 프로그램을 작성하시오.
+
+예를 들어, 수열 A = {10, 20, 10, 30, 20, 50} 인 경우에 가장 긴 증가하는 부분 수열은 A = {10, 20, 10, 30, 20, 50} 이고, 길이는 4이다.
+
+입력
+첫째 줄에 수열 A의 크기 N (1 ≤ N ≤ 1,000,000)이 주어진다.
+
+둘째 줄에는 수열 A를 이루고 있는 Ai가 주어진다. (1 ≤ Ai ≤ 1,000,000)
+
+출력
+첫째 줄에 수열 A의 가장 긴 증가하는 부분 수열의 길이를 출력한다.
+
+예제 입력 1
+6
+10 20 10 30 20 50
+예제 출력 1
+4
+ */
+package baekjoon.year2025.march;
+import java.io.*;
+import java.util.*;
+
+public class BOJ12015 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // LIS 배열을 위한 리스트
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            int num = A[i];
+
+            if (list.isEmpty() || list.get(list.size() - 1) < num) {
+                list.add(num);
+            } else {
+                // 이분탐색으로 현재 숫자가 들어갈 위치 찾기
+                int left = 0;
+                int right = list.size() - 1;
+                while (left < right) {
+                    int mid = (left + right) / 2;
+                    if (list.get(mid) < num) {
+                        left = mid + 1;
+                    } else {
+                        right = mid;
+                    }
+                }
+                list.set(right, num);  // 덮어쓰기
+            }
+        }
+
+        System.out.println(list.size());  // LIS의 길이 출력
+    }
+}
