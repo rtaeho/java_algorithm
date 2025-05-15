2e938ef63fb4a03dec7d37363bd1d76cd3f8f5ae
류태호 <ryou0920@naver.com>
2025-05-15 22:48:14 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 388 ms, Memory: 120940 KB -BaekjoonHub

---
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.java
@@ -0,0 +1,80 @@
+/*
+문제
+예제를 보고 규칙을 유추한 뒤에 별을 찍어 보세요.
+
+입력
+첫째 줄에 N이 주어진다. N은 항상 3×2k 수이다. (3, 6, 12, 24, 48, ...) (0 ≤ k ≤ 10, k는 정수)
+
+출력
+첫째 줄부터 N번째 줄까지 별을 출력한다.
+
+예제 입력 1
+24
+예제 출력 1
+                       *
+                      * *
+                     *****
+                    *     *
+                   * *   * *
+                  ***** *****
+                 *           *
+                * *         * *
+               *****       *****
+              *     *     *     *
+             * *   * *   * *   * *
+            ***** ***** ***** *****
+           *                       *
+          * *                     * *
+         *****                   *****
+        *     *                 *     *
+       * *   * *               * *   * *
+      ***** *****             ***** *****
+     *           *           *           *
+    * *         * *         * *         * *
+   *****       *****       *****       *****
+  *     *     *     *     *     *     *     *
+ * *   * *   * *   * *   * *   * *   * *   * *
+***** ***** ***** ***** ***** ***** ***** *****
+ */
+import java.io.*;
+
+public class Main {
+    static char[][] map;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        map = new char[N][2 * N - 1];
+
+        // 공백 초기화
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < 2 * N - 1; j++) {
+                map[i][j] = ' ';
+            }
+        }
+
+        draw(N, 0, N - 1);  // 시작점 (0, N-1) 기준으로
+        StringBuilder sb = new StringBuilder();
+        for (char[] row : map) {
+            sb.append(row).append('\n');
+        }
+        System.out.print(sb);
+    }
+
+    static void draw(int n, int x, int y) {
+        if (n == 3) {
+            map[x][y] = '*';
+            map[x + 1][y - 1] = '*';
+            map[x + 1][y + 1] = '*';
+            for (int i = -2; i <= 2; i++) {
+                map[x + 2][y + i] = '*';
+            }
+            return;
+        }
+
+        int half = n / 2;
+        draw(half, x, y);                    // 상단 삼각형
+        draw(half, x + half, y - half);      // 좌측 하단
+        draw(half, x + half, y + half);      // 우측 하단
+    }
+}
