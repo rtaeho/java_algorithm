57db43ffa03cfca754066a84f2f3d819472e26f7
rtaeho <ryou0920@naver.com>
2025-05-15 22:47:50 +0900

    Update BOJ2448.java

--- src/baekjoon/year2025/march/BOJ2448.java
+++ src/baekjoon/year2025/march/BOJ2448.java
@@ -38,5 +38,45 @@
  */
 package baekjoon.year2025.march;
 
+import java.io.*;
+
 public class BOJ2448 {
+    static char[][] map;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        map = new char[N][2 * N - 1];
+
+        // 공백 초기화
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < 2 * N - 1; j++) {
+                map[i][j] = ' ';
+            }
+        }
+
+        draw(N, 0, N - 1);  // 시작점 (0, N-1) 기준으로
+        StringBuilder sb = new StringBuilder();
+        for (char[] row : map) {
+            sb.append(row).append('\n');
+        }
+        System.out.print(sb);
+    }
+
+    static void draw(int n, int x, int y) {
+        if (n == 3) {
+            map[x][y] = '*';
+            map[x + 1][y - 1] = '*';
+            map[x + 1][y + 1] = '*';
+            for (int i = -2; i <= 2; i++) {
+                map[x + 2][y + i] = '*';
+            }
+            return;
+        }
+
+        int half = n / 2;
+        draw(half, x, y);                    // 상단 삼각형
+        draw(half, x + half, y - half);      // 좌측 하단
+        draw(half, x + half, y + half);      // 우측 하단
+    }
 }
