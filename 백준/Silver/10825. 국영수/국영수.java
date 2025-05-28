0c6d6217a8d2de48addbae126ffa9e0a4c1810f4
류태호 <ryou0920@naver.com>
2025-05-28 20:46:06 +0900

    [Silver IV] Title: 국영수, Time: 668 ms, Memory: 60164 KB -BaekjoonHub

---
+++ 백준/Silver/10825. 국영수/국영수.java
@@ -0,0 +1,98 @@
+/*
+문제
+도현이네 반 학생 N명의 이름과 국어, 영어, 수학 점수가 주어진다. 이때, 다음과 같은 조건으로 학생의 성적을 정렬하는 프로그램을 작성하시오.
+
+국어 점수가 감소하는 순서로
+국어 점수가 같으면 영어 점수가 증가하는 순서로
+국어 점수와 영어 점수가 같으면 수학 점수가 감소하는 순서로
+모든 점수가 같으면 이름이 사전 순으로 증가하는 순서로 (단, 아스키 코드에서 대문자는 소문자보다 작으므로 사전순으로 앞에 온다.)
+입력
+첫째 줄에 도현이네 반의 학생의 수 N (1 ≤ N ≤ 100,000)이 주어진다. 둘째 줄부터 한 줄에 하나씩 각 학생의 이름, 국어, 영어, 수학 점수가 공백으로 구분해 주어진다. 점수는 1보다 크거나 같고, 100보다 작거나 같은 자연수이다. 이름은 알파벳 대소문자로 이루어진 문자열이고, 길이는 10자리를 넘지 않는다.
+
+출력
+문제에 나와있는 정렬 기준으로 정렬한 후 첫째 줄부터 N개의 줄에 걸쳐 각 학생의 이름을 출력한다.
+
+예제 입력 1
+12
+Junkyu 50 60 100
+Sangkeun 80 60 50
+Sunyoung 80 70 100
+Soong 50 60 90
+Haebin 50 60 100
+Kangsoo 60 80 100
+Donghyuk 80 60 100
+Sei 70 70 70
+Wonseob 70 70 90
+Sanghyun 70 70 80
+nsj 80 80 80
+Taewhan 50 60 90
+예제 출력 1
+Donghyuk
+Sangkeun
+Sunyoung
+nsj
+Wonseob
+Sanghyun
+Sei
+Kangsoo
+Haebin
+Junkyu
+Soong
+Taewhan
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static class Student {
+        String name;
+        int korean, english, math;
+
+        public Student(String name, int korean, int english, int math) {
+            this.name = name;
+            this.korean = korean;
+            this.english = english;
+            this.math = math;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        List<Student> students = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            int korean = Integer.parseInt(st.nextToken());
+            int english = Integer.parseInt(st.nextToken());
+            int math = Integer.parseInt(st.nextToken());
+
+            students.add(new Student(name, korean, english, math));
+        }
+
+        students.sort((a, b) -> {
+            if (a.korean != b.korean) {
+                return b.korean - a.korean;                   // 국어 감소
+            }
+            if (a.english != b.english) {
+                return a.english - b.english;              // 영어 증가
+            }
+            if (a.math != b.math) {
+                return b.math - a.math;                          // 수학 감소
+            }
+            return a.name.compareTo(b.name);                                       // 이름 사전순
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for (Student s : students) {
+            sb.append(s.name).append('\n');
+        }
+
+        System.out.print(sb);
+    }
+}
