267fb63735d7da68bd97754e1b72cab01ed65400
rtaeho <ryou0920@naver.com>
2025-06-02 16:58:54 +0900

    Update BOJ1377.java

--- src/baekjoon/year2025/june/BOJ1377.java
+++ src/baekjoon/year2025/june/BOJ1377.java
@@ -1,2 +1,87 @@
-package baekjoon.year2025.june;public class BOJ1377 {
+/*
+문제
+버블 소트 알고리즘을 다음과 같이 C++로 작성했다.
+
+bool changed = false;
+for (int i=1; i<=N+1; i++) {
+    changed = false;
+    for (int j=1; j<=N-i; j++) {
+        if (A[j] > A[j+1]) {
+            changed = true;
+            swap(A[j], A[j+1]);
+        }
+    }
+    if (changed == false) {
+        cout << i << '\n';
+        break;
+    }
+}
+위 소스에서 N은 배열의 크기이고, A는 정렬해야 하는 배열이다. 배열은 A[1]부터 사용한다.
+
+위와 같은 소스를 실행시켰을 때, 어떤 값이 출력되는지 구해보자.
+
+입력
+첫째 줄에 N이 주어진다. N은 500,000보다 작거나 같은 자연수이다. 둘째 줄부터 N개의 줄에 A[1]부터 A[N]까지 하나씩 주어진다. A에 들어있는 수는 1,000,000보다 작거나 같은 자연수 또는 0이다.
+
+출력
+정답을 출력한다.
+
+예제 입력 1
+5
+10
+1
+5
+2
+3
+예제 출력 1
+3
+예제 입력 2
+5
+1
+3
+5
+7
+9
+예제 출력 2
+1
+ */
+package baekjoon.year2025.june;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class BOJ1377 {
+
+    static class Pair {
+        int value;
+        int index;
+
+        public Pair(int value, int index) {
+            this.value = value;
+            this.index = index;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Pair[] arr = new Pair[N];
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+            arr[i] = new Pair(num, i);
+        }
+
+        Arrays.sort(arr, Comparator.comparingInt(p -> p.value));
+
+        int maxMove = 0;
+        for (int i = 0; i < N; i++) {
+            int move = arr[i].index - i;
+            maxMove = Math.max(maxMove, move);
+        }
+
+        System.out.println(maxMove + 1);
+    }
 }
