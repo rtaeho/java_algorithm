2682b41b8c2b6c43af412e9ce335135966aa4cfa
rtaeho <ryou0920@naver.com>
2025-06-18 15:32:37 +0900

    Update BOJ1654.java

--- src/baekjoon/year2025/june/BOJ2805.java
+++ src/baekjoon/year2025/june/BOJ2805.java
@@ -25,5 +25,15 @@
 예제 출력 2 
 36
  */
+package baekjoon.year2025.june;
 
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ2805 {
+    public static void main(String[] args) throws IOException {
+
+    }
+}
