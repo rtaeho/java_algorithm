d6a6ccc11640e3f060dbfbcd0392adee2cf646b3
류태호 <ryou0920@naver.com>
2025-09-06 20:27:45 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 2.99 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array, int n) {
+        int answer = (int) Arrays.stream(array)
+                          .filter(x -> x == n)
+                          .count();
+
+        return answer;
+    }
+}
