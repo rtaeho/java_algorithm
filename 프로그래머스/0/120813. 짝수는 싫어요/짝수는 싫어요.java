0f3f604bb58985405f10bd7c6ca91b1528278e87
류태호 <ryou0920@naver.com>
2025-09-12 21:26:07 +0900

    [level 0] Title: 짝수는 싫어요, Time: 5.22 ms, Memory: 80 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,11 @@
+import java.util.stream.*;
+
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = {};
+        answer = IntStream.rangeClosed(1, n)
+                        .filter(i -> i % 2 != 0)
+                        .toArray();
+        return answer;
+    }
+}
