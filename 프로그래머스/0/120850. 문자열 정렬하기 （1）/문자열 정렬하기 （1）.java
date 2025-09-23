ab8051e663a1ab51bce4f50f66d9d63c8f64c49d
류태호 <ryou0920@naver.com>
2025-09-23 23:29:04 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 4.18 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+class Solution {
+    public int[] solution(String my_string) {
+        int[] answer = {};
+        answer = Arrays.stream(my_string.replaceAll("[^0-9]", "").split(""))
+            .mapToInt(Integer::parseInt).sorted().toArray();
+        return answer;
+    }
+}
