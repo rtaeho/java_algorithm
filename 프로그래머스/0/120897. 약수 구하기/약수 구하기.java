53c314451aaf11051cc7e8bc4bca333567011844
류태호 <ryou0920@naver.com>
2025-10-03 22:47:55 +0900

    [level 0] Title: 약수 구하기, Time: 0.17 ms, Memory: 85.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = {};
+        List<Integer> arr = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                arr.add(i);
+            }
+        }
+        answer = new int[arr.size()];
+        for(int i = 0; i < arr.size(); i++) {
+            answer[i] = arr.get(i);
+        }
+        return answer;
+    }
+}
