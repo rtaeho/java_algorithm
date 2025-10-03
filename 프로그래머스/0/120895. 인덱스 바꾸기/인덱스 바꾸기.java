85664348884525c6e92843c41b9ecf5a67eea0f5
류태호 <ryou0920@naver.com>
2025-10-03 21:41:45 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.05 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < my_string.length(); i++){
+            if(i == num2){
+                sb.append(my_string.charAt(num1));
+            }
+            else if (i == num1){
+                sb.append(my_string.charAt(num2));
+            }
+            else{
+                sb.append(my_string.charAt(i));   
+            }
+        }
+        answer = sb.toString();
+        return answer;
+    }
+}
