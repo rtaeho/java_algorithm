0d5a4f429298baef549e7907238168b5182e2768
류태호 <ryou0920@naver.com>
2025-10-03 21:10:34 +0900

    [level 0] Title: 암호 해독, Time: 0.23 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String cipher, int code) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        for(int i=1; i<cipher.length() + 1; i++){
+            if(i % code == 0){
+                sb.append(cipher.charAt(i-1));
+            }
+        }
+        answer = sb.toString();
+        return answer;
+    }
+}
