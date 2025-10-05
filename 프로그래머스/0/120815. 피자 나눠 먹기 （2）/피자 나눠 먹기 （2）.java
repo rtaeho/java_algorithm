290028c4021e7c40b8214d99c93dc3007bffa929
류태호 <ryou0920@naver.com>
2025-10-05 15:56:15 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.03 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -1,11 +1,22 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        answer = 6;
-        while(answer%n!=0){
-            answer+=6;
-        }
-        answer /= 6;
+        answer = lcm(n, 6) / 6;
         return answer;
+    }
+    
+    // 최소공배수 = a*b/최대공약수(a,b)
+    private int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
+    }
+
+    // 최대공약수
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
     }
 }
