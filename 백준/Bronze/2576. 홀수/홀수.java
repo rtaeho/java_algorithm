1fb3e55cf0dbdf9ba58e4b672719aa4d77dc1864
류태호 <ryou0920@naver.com>
2025-12-08 23:53:13 +0900

    [Bronze III] Title: 홀수, Time: 100 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Bronze/2576. 홀수/홀수.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int sumOfOdds = 0;
+        int minOdd = 101;
+
+        for (int i = 0; i < 7; i++) {
+            int number = Integer.parseInt(br.readLine());
+
+            if (number % 2 != 0) {
+                sumOfOdds += number;
+
+                if (number < minOdd) {
+                    minOdd = number;
+                }
+            }
+        }
+
+        if (sumOfOdds == 0) {
+            System.out.println("-1");
+        } else {
+            System.out.println(sumOfOdds);
+            System.out.println(minOdd);
+        }
+    }
+}
