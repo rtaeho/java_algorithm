dc1c70087d4fe94c50ce46329294424be6770855
rtaeho <ryou0920@naver.com>
2025-12-08 23:51:26 +0900

    Update BOJ2490.java

--- src/baekjoon/year2025/december/BOJ2490.java
+++ src/baekjoon/year2025/december/BOJ2490.java
@@ -3,22 +3,25 @@ package baekjoon.year2025.december;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class BOJ2490 {
     public static void main(String[] args) throws IOException {
-        // 도 A 개 B 걸 C 모 E
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        ;
+
         for (int i = 0; i < 3; i++) {
             String s = br.readLine();
-            String[] tmp = s.split(" ");
-            int cnt = 0;
+            StringTokenizer st = new StringTokenizer(s, " ");
+
+            int cntZero = 0;
+
             for (int j = 0; j < 4; j++) {
-                if (tmp[j].equals("0")) {
-                    cnt++;
+                if (st.nextToken().equals("0")) {
+                    cntZero++;
                 }
             }
-            switch (cnt) {
+
+            switch (cntZero) {
                 case 0:
                     System.out.println("E");
                     break;
@@ -27,6 +30,12 @@ public class BOJ2490 {
                     break;
                 case 2:
                     System.out.println("B");
+                    break;
+                case 3:
+                    System.out.println("C");
+                    break;
+                case 4:
+                    System.out.println("D");
                     break;
             }
         }
