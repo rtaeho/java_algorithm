aa9c751648a3619e959b0f7dd9197b3123f84182
rtaeho <ryou0920@naver.com>
2025-12-10 21:40:25 +0900

    Create BOJ1267.java

---
+++ src/baekjoon/year2025/december/BOJ1267.java
@@ -0,0 +1,35 @@
+package baekjoon.year2025.december;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class BOJ1267 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int youngsikTotalCost = 0;
+        int minsikTotalCost = 0;
+
+        for (int i = 0; i < N; i++) {
+            int T = Integer.parseInt(st.nextToken());
+
+            youngsikTotalCost += (T / 30 + 1) * 10;
+
+            minsikTotalCost += (T / 60 + 1) * 15;
+        }
+
+        if (youngsikTotalCost < minsikTotalCost) {
+            System.out.println("Y " + youngsikTotalCost);
+        } else if (minsikTotalCost < youngsikTotalCost) {
+            System.out.println("M " + minsikTotalCost);
+        } else {
+            System.out.println("Y M " + youngsikTotalCost);
+        }
+    }
+}
