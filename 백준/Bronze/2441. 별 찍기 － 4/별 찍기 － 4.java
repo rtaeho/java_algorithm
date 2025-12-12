26730b72249ee347c5caaeed3699f4db3bdb3da5
류태호 <ryou0920@naver.com>
2025-12-12 23:31:59 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 260 ms, Memory: 17600 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,21 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = i; k < n; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
