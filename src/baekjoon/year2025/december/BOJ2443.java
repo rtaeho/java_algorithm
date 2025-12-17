ea3c677e64c792e2eae20f5e43dc013e5cf2dfbc
rtaeho <ryou0920@naver.com>
2025-12-17 22:30:24 +0900

    Create BOJ2443.java

---
+++ src/baekjoon/year2025/december/BOJ2443.java
@@ -0,0 +1,25 @@
+package baekjoon.year2025.december;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class BOJ2443 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i - 1; j++) {
+                sb.append(" ");
+            }
+            for (int k = 1; k <= 2 * (N - i) + 1; k++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+
+        System.out.print(sb.toString());
+    }
+}
