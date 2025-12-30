471da2fd6e0b94ab0093c54a6597d826e63bd55f
류태호 <ryou0920@naver.com>
2025-12-30 12:57:45 +0900

    [Silver V] Title: 방 번호, Time: 104 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String n = br.readLine();
+
+        int[] count = new int[10];
+        for (int i = 0; i < n.length(); i++) {
+            int num = n.charAt(i) - '0';
+            count[num]++;
+        }
+
+        int sn = (count[6] + count[9] + 1) / 2;
+        count[6] = sn;
+        count[9] = sn;
+
+        int max = 0;
+        for (int i = 0; i < 10; i++) {
+            if (count[i] > max) {
+                max = count[i];
+            }
+        }
+
+        System.out.println(max);
+    }
+}
