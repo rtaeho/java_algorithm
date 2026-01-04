9947ff2cd592216715fa7f85bf2e18f7e66cb15d
rtaeho <ryou0920@naver.com>
2026-01-04 23:34:26 +0900

    Create BOJ1919.java

---
+++ src/baekjoon/year2026/BOJ1919.java
@@ -0,0 +1,32 @@
+package baekjoon.year2026;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class BOJ1919 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s1 = br.readLine();
+        String s2 = br.readLine();
+
+        int[] count1 = new int[26];
+        int[] count2 = new int[26];
+
+        for (int i = 0; i < s1.length(); i++) {
+            count1[s1.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < s2.length(); i++) {
+            count2[s2.charAt(i) - 'a']++;
+        }
+
+        int result = 0;
+        for (int i = 0; i < 26; i++) {
+            result += Math.abs(count1[i] - count2[i]);
+        }
+
+        System.out.println(result);
+    }
+}
