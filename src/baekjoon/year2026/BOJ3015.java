cc5acbbb8feb451012c7daf6df32e6fc3fe106e1
rtaeho <ryou0920@naver.com>
2026-01-08 13:12:35 +0900

    Update BOJ3015.java

--- src/baekjoon/year2026/BOJ3015.java
+++ src/baekjoon/year2026/BOJ3015.java
@@ -9,10 +9,28 @@ public class BOJ3015 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));;
         int n = Integer.parseInt(br.readLine());
-        Stack<Integer> stack = new Stack<>();
+        int result = 0;
+        Stack<int[]> stack = new Stack<>();
 
         for (int i = 0; i < n; i++) {
-            stack.push(Integer.parseInt(br.readLine()));
+            int height = Integer.parseInt(br.readLine());
+            int cnt = 1;
+            while(!stack.isEmpty() && stack.peek()[0] <= height){
+                int[] top = stack.pop();
+                result += top[1];
+
+                if (top[0] == height) { // 같으면 한 사람으로 취급
+                    cnt += top[1];
+                }
+                // 작은 경우는 위에서 pop 했기에 별도 처리 불필요
+
+            }
+            if(!stack.isEmpty()) { // 남아있으면
+                result++;
+            }
+            stack.push(new int[]{height, cnt}); // 그 후 내 값 넣음
         }
+
+        System.out.println(result);
     }
 }
