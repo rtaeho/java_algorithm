987acc06bb8d18c6cdf20dec74faf033ed24964b
rtaeho <ryou0920@naver.com>
2026-01-08 13:20:01 +0900

    Update BOJ3015.java

--- src/baekjoon/year2026/BOJ3015.java
+++ src/baekjoon/year2026/BOJ3015.java
@@ -7,7 +7,7 @@ import java.util.Stack;
 
 public class BOJ3015 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         long result = 0;
         Stack<int[]> stack = new Stack<>();
