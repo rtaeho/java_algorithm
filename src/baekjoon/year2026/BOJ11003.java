82dbf4647c59bc09dc58ed296d34bf4408f90b16
rtaeho <ryou0920@naver.com>
2026-01-13 12:52:55 +0900

    Update BOJ11003.java

--- src/baekjoon/year2026/BOJ11003.java
+++ src/baekjoon/year2026/BOJ11003.java
@@ -1,34 +1,44 @@
 package baekjoon.year2026;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class BOJ11003 {
+    static class Node implements Comparable<Node> {
+        int value;
+        int index;
+
+        Node(int value, int index) {
+            this.value = value;
+            this.index = index;
+        }
+        // 기본 비교 메서드 오버라이딩
+        @Override
+        public int compareTo(Node other) {
+            return Integer.compare(this.value, other.value);
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int l = Integer.parseInt(st.nextToken());
-        int[] arr = new int[n];
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            int start = Math.max(0, i - l + 1);
-            int min = Integer.MAX_VALUE;
-            for (int j = start; j <= i; j++) {
-                if (arr[j] < min) {
-                    min = arr[j];
-                }
+            int now = Integer.parseInt(st.nextToken());
+            pq.add(new Node(now, i)); // compareTo를 통하여 add
+
+            while (!pq.isEmpty() && pq.peek().index <= i - l) {
+                pq.poll();
             }
-            sb.append(min).append(" ");
+
+            sb.append(pq.peek().value).append(" ");
         }
 
         System.out.println(sb.toString());
-
     }
 }
