55d50fd93e6090f48745328b91af49c5bc27731e
류태호 <ryou0920@naver.com>
2026-01-13 12:57:25 +0900

    [Gold I] Title: 최솟값 찾기, Time: 1752 ms, Memory: 433128 KB -BaekjoonHub

---
+++ 백준/Gold/11003. 최솟값 찾기/최솟값 찾기.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int l = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int[] dequeIdx = new int[n];
+        int head = 0;
+        int tail = 0;
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            while (tail > head && arr[dequeIdx[tail - 1]] >= arr[i]) {
+                tail--;
+            }
+            dequeIdx[tail++] = i;
+            if (dequeIdx[head] <= i - l) {
+                head++;
+            }
+            sb.append(arr[dequeIdx[head]]).append(" ");
+        }
+        System.out.println(sb.toString());
+    }
+}
