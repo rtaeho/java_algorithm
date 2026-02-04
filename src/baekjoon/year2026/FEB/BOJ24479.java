d5e7ff4908f3dc5888832d866ca0b85d3908b33c
rtaeho <ryou0920@naver.com>
2026-02-04 10:14:36 +0900

    Create BOJ24479.java

---
+++ src/baekjoon/year2026/FEB/BOJ24479.java
@@ -0,0 +1,55 @@
+package baekjoon.year2026.FEB;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class BOJ24479 {
+    static int N, M, R;
+    static ArrayList<Integer>[] graph;
+    static int[] visited;
+    static int cnt = 1;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+        graph = new ArrayList[N + 1];
+        visited = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+        // 오름차순 방문을 위한 정렬
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(graph[i]);
+        }
+        dfs(R);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            sb.append(visited[i]).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    static void dfs(int start) {
+        visited[start] = cnt++;
+        for (int next : graph[start]) {
+            if (visited[next] == 0) {
+                dfs(next);
+            }
+        }
+    }
+}
