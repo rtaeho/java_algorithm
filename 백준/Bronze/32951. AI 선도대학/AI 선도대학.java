1c43063991648e58b71d46d7a0204b586db0968d
류태호 <ryou0920@naver.com>
2026-03-18 16:15:35 +0900

    [Bronze V] Title: AI 선도대학, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/32951. AI 선도대학/AI 선도대학.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));       
+        System.out.println(Integer.parseInt(br.readLine()) - 2024);
+    }
+}
