9dd8fae2c5d75c60aa3547faad32b32121481212
류태호 <ryou0920@naver.com>
2026-03-30 11:42:30 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,7 @@
+-- 코드를 작성해주세요
+select ID,EMAIL, FIRST_NAME, LAST_NAME
+from developers
+where skill_code & (select sum(code) 
+					from skillcodes
+					where name in('Python','C#'))
+order by id asc
