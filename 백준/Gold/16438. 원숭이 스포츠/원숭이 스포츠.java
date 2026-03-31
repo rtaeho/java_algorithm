a2821eac10523e1502388b6ebefc29b9f1a111b8
류태호 <ryou0920@naver.com>
2026-03-31 12:15:44 +0900

    [Gold III] Title: 원숭이 스포츠, Time: 108 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Gold/16438. 원숭이 스포츠/원숭이 스포츠.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int N;
+    static char[][] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+        if (line == null) {
+            return;
+        }
+
+        N = Integer.parseInt(line);
+        arr = new char[7][N];
+
+        solve(0, 0, N);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 7; i++) {
+            boolean hasA = false;
+            boolean hasB = false;
+
+            for (int j = 0; j < N; j++) {
+                if (arr[i][j] == 'A') {
+                    hasA = true;
+                }
+                if (arr[i][j] == 'B') {
+                    hasB = true;
+                }
+            }
+
+            if (!hasA) {
+                arr[i][N - 1] = 'A';
+            }
+            if (!hasB) {
+                arr[i][N - 1] = 'B';
+            }
+
+            for (int j = 0; j < N; j++) {
+                sb.append(arr[i][j]);
+            }
+            sb.append('\n');
+        }
+        System.out.print(sb);
+    }
+
+    static void solve(int day, int start, int end) {
+        if (day == 7) {
+            return;
+        }
+
+        int mid = (start + end) / 2;
+
+        for (int i = start; i < mid; i++) {
+            arr[day][i] = 'A';
+        }
+        for (int i = mid; i < end; i++) {
+            arr[day][i] = 'B';
+        }
+
+        solve(day + 1, start, mid);
+        solve(day + 1, mid, end);
+    }
+}
