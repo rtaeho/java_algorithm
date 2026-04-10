484a13b6b6015f5ffea3084a5f2511f8a2fcb683
류태호 <ryou0920@naver.com>
2026-04-10 21:33:13 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -0,0 +1,17 @@
+SELECT 
+    I.ID, 
+    N.FISH_NAME, 
+    I.LENGTH
+FROM 
+    FISH_INFO I
+JOIN 
+    FISH_NAME_INFO N ON I.FISH_TYPE = N.FISH_TYPE
+WHERE 
+    (I.FISH_TYPE, I.LENGTH) IN (
+        -- 종류별로 가장 큰 길이를 먼저 찾음
+        SELECT FISH_TYPE, MAX(LENGTH)
+        FROM FISH_INFO
+        GROUP BY FISH_TYPE
+    )
+ORDER BY 
+    I.ID ASC;
