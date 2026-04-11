9c0ff93047279675653253f6400109477a82bb3b
류태호 <ryou0920@naver.com>
2026-04-11 19:28:12 +0900

    [Bronze V] Title: Parking Pandemonium, Time: 104 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/34297. Parking Pandemonium/Parking Pandemonium.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int m = Integer.parseInt(br.readLine());
+        br.readLine();
+        int c = Integer.parseInt(br.readLine());
+        
+        System.out.println(m * c);
+    }
+}
