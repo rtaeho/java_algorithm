1a3935a580d23cbfa81bdcbf9f923c0907105a2f
류태호 <ryou0920@naver.com>
2026-04-16 21:31:20 +0900

    [Gold I] Title: K번째 수, Time: 128 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.java
+++ 백준/Gold/1300. K번째 수/K번째 수.java
@@ -1,21 +1,3 @@
-/*
- * 문제
-세준이는 크기가 N×N인 배열 A를 만들었다. 배열에 들어있는 수 A[i][j] = i×j 이다. 이 수를 일차원 배열 B에 넣으면 B의 크기는 N×N이 된다. B를 오름차순 정렬했을 때, B[k]를 구해보자.
-
-배열 A와 B의 인덱스는 1부터 시작한다.
-
-입력
-첫째 줄에 배열의 크기 N이 주어진다. N은 105보다 작거나 같은 자연수이다. 둘째 줄에 k가 주어진다. k는 min(109, N2)보다 작거나 같은 자연수이다.
-
-출력
-B[k]를 출력한다.
-
-예제 입력 1 
-3
-7
-예제 출력 1 
-6
- */
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -30,12 +12,16 @@ public class Main {
         long right = k;
         long answer = 0;
 
-        // 이분 탐색
         while (left <= right) {
+            // i행의 j열은 i * j니까 i번째 행은 i, 2*i, 3*i 이런식
+            // 이 중 mid 이하인 애들은 i * j <= mid 이고
+            // j <= mid / i 이니까 j의 최대는 mid / i
+
             long mid = (left + right) / 2;
             long count = 0;
 
-            // mid보다 작거나 같은 수의 개수 계산
+            // i행에 mid 이하인 애들은 mid / i개
+            // 어차피 N보다는 못 커지니까 min으로 잘라줌
             for (int i = 1; i <= N; i++) {
                 count += Math.min(mid / i, N);
             }
@@ -48,6 +34,6 @@ public class Main {
             }
         }
 
-        System.out.println(answer);
+        System.out.print(answer);
     }
 }
