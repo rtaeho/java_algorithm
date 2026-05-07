5efa4412254dad75e602b555c3baf16d65c1c4c2
류태호 <ryou0920@naver.com>
2026-05-07 10:35:57 +0900

    [level 3] Title: 베스트앨범, Time: 4.45 ms, Memory: 54.8 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,63 +1,58 @@
 import java.util.*;
 
 class Solution {
-    static class Song {
-        int id;
+    static class Song{
         int play;
-
-        public Song(int id, int play) {
-            this.id = id;
+        int idx;
+        
+        public Song(int play, int idx){
             this.play = play;
+            this.idx = idx;
         }
     }
-
-    public int[] solution(String[] genres, int[] plays) {
-        HashMap<String, Integer> genrePlayCount = new HashMap<>();
-        HashMap<String, List<Song>> genreMap = new HashMap<>();
-
-        for (int i = 0; i < genres.length; i++) {
-            genrePlayCount.put(genres[i], genrePlayCount.getOrDefault(genres[i], 0) + plays[i]);
-            
-            if (!genreMap.containsKey(genres[i])) {
-                genreMap.put(genres[i], new ArrayList<>());
-            }
-            genreMap.get(genres[i]).add(new Song(i, plays[i]));
-        }
-
-        List<String> sortedGenres = new ArrayList<>(genrePlayCount.keySet());
-        Collections.sort(sortedGenres, new Comparator<String>() {
-            @Override
-            public int compare(String g1, String g2) {
-                return genrePlayCount.get(g2) - genrePlayCount.get(g1);
-            }
-        });
-
-        List<Integer> bestAlbumList = new ArrayList<>();
-
-        for (String genre : sortedGenres) {
-            List<Song> songs = genreMap.get(genre);
-            
-            Collections.sort(songs, new Comparator<Song>() {
-                @Override
-                public int compare(Song s1, Song s2) {
-                    if (s1.play == s2.play) {
-                        return s1.id - s2.id;
-                    }
-                    return s2.play - s1.play;
+    static class Rank{
+        int total;
+        PriorityQueue<Song> pq;
+        
+        public Rank(){
+            this.pq = new PriorityQueue<>((s1, s2) ->{
+                if(s1.play == s2.play){
+                    return Integer.compare(s1.idx, s2.idx);
                 }
+                return Integer.compare(s2.play, s1.play);
             });
-
-            bestAlbumList.add(songs.get(0).id);
-            if (songs.size() > 1) {
-                bestAlbumList.add(songs.get(1).id);
+            this.total = 0;
+        }
+    }
+    public int[] solution(String[] genres, int[] plays) {
+        HashMap<String, Rank> map = new HashMap<>();
+        for(int i = 0; i < genres.length; i++){
+            String genre = genres[i];
+            int playCnt = plays[i];
+            Rank rank = map.getOrDefault(genre, new Rank());
+            rank.total += playCnt;
+            rank.pq.offer(new Song(playCnt, i));
+            map.put(genre, rank);
+        }
+        List<Map.Entry<String, Rank>> entryList = new LinkedList<>(map.entrySet());
+        entryList.sort((o1, o2) -> {
+            return Integer.compare(o2.getValue().total, o1.getValue().total);
+        });
+        
+        List<Integer> answerList = new LinkedList<>();
+        int idx = 0;
+        for(Map.Entry<String, Rank> entry : entryList){
+            PriorityQueue<Song> songPQ = entry.getValue().pq;
+            if(songPQ.size() > 1){
+                answerList.add(songPQ.poll().idx);
             }
+            answerList.add(songPQ.poll().idx);
         }
-
-        int[] answer = new int[bestAlbumList.size()];
-        for (int i = 0; i < bestAlbumList.size(); i++) {
-            answer[i] = bestAlbumList.get(i);
+    
+        int[] answer = new int[answerList.size()];
+        for (int i = 0; i < answerList.size(); i++) {
+            answer[i] = answerList.get(i);
         }
-
         return answer;
     }
 }
