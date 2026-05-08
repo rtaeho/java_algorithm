a5d11bccead7a1a1f7c321c18f14bb2b2f968601
류태호 <ryou0920@naver.com>
2026-05-08 09:56:23 +0900

    [level 2] Title: 기능개발, Time: 1.65 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,35 +2,27 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        List<Integer> result = new ArrayList<>();
-        Queue<Integer> days = new LinkedList<>();
-
+        Queue<Integer> q = new LinkedList<>();
+        
         for (int i = 0; i < progresses.length; i++) {
-            int remaining = 100 - progresses[i];
-            int day = remaining / speeds[i];
-            if (remaining % speeds[i] != 0) {
-                day++;
+            int remain = 100 - progresses[i];
+            int day = (remain + speeds[i] - 1) / speeds[i];
+            q.add(day);
+        }
+
+        List<Integer> list = new ArrayList<>();
+        
+        while (!q.isEmpty()) {
+            int now = q.poll();
+            int cnt = 1;
+
+            while (!q.isEmpty() && q.peek() <= now) {
+                q.poll();
+                cnt++;
             }
-            days.add(day);
+            list.add(cnt);
         }
 
-        while (!days.isEmpty()) {
-            int currentDay = days.poll();
-            int count = 1;
-
-            while (!days.isEmpty() && days.peek() <= currentDay) {
-                days.poll();
-                count++;
-            }
-
-            result.add(count);
-        }
-
-        int[] answer = new int[result.size()];
-        for (int i = 0; i < result.size(); i++) {
-            answer[i] = result.get(i);
-        }
-
-        return answer;
+        return list.stream().mapToInt(s->s).toArray();
     }
 }
