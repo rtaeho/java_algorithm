a5917acb7c1eaa6e2d4064f20a2e62f61f1ee32d
류태호 <ryou0920@naver.com>
2026-05-08 11:18:14 +0900

    [level 2] Title: 올바른 괄호, Time: 1.88 ms, Memory: 56.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,16 +1,14 @@
 class Solution {
     boolean solution(String s) {
         int count = 0;
+        int len = s.length();
 
-        for (int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < len; i++) {
             if (s.charAt(i) == '(') {
                 count++;
             } else {
+                if (count == 0) return false;
                 count--;
-            }
-
-            if (count < 0) {
-                return false;
             }
         }
 
