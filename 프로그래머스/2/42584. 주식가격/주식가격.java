a429d83ed6a23272246f68adb49488cbf9f7d235
류태호 <ryou0920@naver.com>
2026-05-18 16:14:58 +0900

    [level 2] Title: 주식가격, Time: 8.53 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,15 +2,14 @@ class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
         
-        for (int i = 0; i < prices.length; i++) {
-            for (int j = i + 1; j < prices.length; j++) {
+        for(int i = 0; i < prices.length - 1; i++){
+            for(int j = i + 1; j < prices.length; j++){
                 answer[i]++;
-                if (prices[i] > prices[j]) {
+                if(prices[i] > prices[j]){
                     break;
                 }
             }
         }
-        
         return answer;
     }
 }
